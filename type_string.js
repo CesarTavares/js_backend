a04fee172ee742e9a346d39d486e7a56eaf0d3cb
Cesar Tavares <63663846+CesarTavares@users.noreply.github.com>
2021-12-04 21:25:13 -0300

    exemplos da aula 1

---
+++ type_string.js
@@ -0,0 +1,25 @@
+const texto1 = "Olá, mundo!";
+const texto2 = 'Olá, mundo!!';
+//const senha = "senhaSuperSegura 456!";
+const StringDeNumeros = "34567";
+
+const citacao = "Meu nome é ";
+const meuNome = "Cesar";
+
+
+//concatenação (+)
+
+console.log(citacao + meuNome);
+
+//template string ou template literal
+
+const senha = "minhaSenha123"
+console.log(senha.length) // 13 caracteres
+
+
+const cidade = "belo horizonte";
+const input = "Belo Horizonte";
+
+const inputMinusculo = input.toLowerCase();
+
+console.log(cidade === inputMinusculo); // true
